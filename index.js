6ce44d5400faa820c098c716021f85cce2a4d920
Kristine Du <kristine.a.du@gmail.com>
2020-09-17 16:19:45 -0400

    Add update and delete functions.

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function createUser(event){
     fetch(USERS_URL, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-type': 'application/json'
         },
         body: JSON.stringify({ username, password })
@@ -34,11 +33,46 @@ function displayUsers(users){
     users.forEach(user => {
         const card = document.createElement('div')
         const username = document.createElement('p')
+        const editUsernameForm = document.createElement('form')
+        const usernameField = document.createElement('input')
+        const submitButton = document.createElement('input')
+        const deleteButton = document.createElement('button')
 
         card.className = 'card'
         username.textContent = user.username
+        usernameField.name = 'username'
+        usernameField.placeholder = 'Update username'
+        submitButton.type = 'submit'
+        deleteButton.textContent = 'Delete User'
 
-        card.append(username)
+        editUsernameForm.append(usernameField, submitButton)
+        card.append(username, editUsernameForm, deleteButton)
         usersContainer.appendChild(card)
+
+        editUsernameForm.addEventListener('submit', (event) => updateUser(event, user, username))
+        deleteButton.addEventListener('click', () => deleteUser(user))
     })
+}
+
+function deleteUser(user){
+    fetch(`${USERS_URL}/${user.id}`, { method: 'DELETE' })
+}
+
+function updateUser(event, user, usernameElement){
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const username = formData.get('username')
+
+    usernameElement.textContent = username
+
+    fetch(`${USERS_URL}/${user.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ username })
+    }).then(response => response.json())
+        .then(console.log)
 }
